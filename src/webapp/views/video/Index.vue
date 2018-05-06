4ab18fa7e8290bcfd0307972a56282eeb1d2f688
xuetong <1129093459@qq.com>
2018-05-06 14:12:59 +0800

    video

---
+++ src/webapp/views/video/Index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <headerComponent></headerComponent>
+        <videoComponent></videoComponent>
+        <footerComponent></footerComponent>
+    </div>
+</template>
+ 
+<script>
+import headerComponent from "../../components/header/Header.vue";
+import footerComponent from "../../components/footer/Footer.vue";
+import videoComponent from "../../components/video/Video.vue";
+
+const fetchInitialData = ({ store }) => {
+  return store.dispatch("getUserInfo");
+};
+
+export default {
+  components: {headerComponent, footerComponent,videoComponent},
+  name: "UserInfo",
+  asyncData: fetchInitialData,
+  mounted() {
+    fetchInitialData({ store: this.$store });
+  }
+};
+</script>
+
+
+
+
