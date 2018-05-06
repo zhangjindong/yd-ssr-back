06386479e13f05a4b0ab3d88cd9ce026e593ab10
xuetong <1129093459@qq.com>
2018-05-06 14:11:06 +0800

    video

--- src/nodeuii/config/local.js
+++ src/nodeuii/config/local.js
@@ -3,7 +3,7 @@ import path from 'path';
 const localConfig = {
 	"RDS_HOST": '192.168.0.33', //服务器IP
 	"RDS_PWD": '123456', //密码
-	"userLoginURL": 'http://test.yidengxuetang.com:8085/users/login',
+	"userLoginURL": 'http://dev.yidengxuetang.com:8085/users/login',
 	"ydWebUrl": "http://127.0.0.1:8085",
 	"apiUrl": "http://192.168.0.32:8080/",
 	"debug": true
